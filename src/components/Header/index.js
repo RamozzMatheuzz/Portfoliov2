f97f493378161535e1ceefc309ce9b7a1b577c10
Matheus <matheusramoses@gmail.com>
2024-08-02 22:34:21 -0300

    startando o git

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import Images2 from '../LoadingImg';
+
+function Header (){
+    return(
+        <>
+            <img src={Images2.svgLogo} alt=""/>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/LoadingImg/index.js
@@ -0,0 +1,47 @@
+import svgCode from '../../img/svg/code.svg';
+import svgCode2 from '../../img/svg/code2.svg';
+import svgCss from '../../img/svg/css.svg';
+import svgFigma from '../../img/svg/figma.svg';
+import svgGithub from '../../img/svg/github.svg';
+import svgGithub2 from '../../img/svg/github2.svg';
+import svgHtml from '../../img/svg/html.svg';
+import svgIllustrator from '../../img/svg/illustrator.svg';
+import svgInstagram from '../../img/svg/Instagram.svg';
+import svgJs from '../../img/svg/js.svg';
+import svgLinkedin from '../../img/svg/linkedin.svg';
+import svgLogo from '../../img/svg/Logo.svg';
+import svgMail from '../../img/svg/mail.svg';
+import svgNode from '../../img/svg/node.svg';
+import svgPhotoshop from '../../img/svg/photoshop.svg';
+import svgReact from '../../img/svg/react.svg';
+import svgSai from '../../img/svg/sai.svg';
+import svgSass from '../../img/svg/sass.svg';
+import svgTiktok from '../../img/svg/Tiktok.svg';
+import svgTwiiter from '../../img/svg/twiiter.svg';
+import svgWhatsapp from '../../img/svg/whatsapp.svg';
+
+const Images = {
+    svgCode: svgCode,
+    svgCode2: svgCode2,
+    svgCss: svgCss,
+    svgFigma: svgFigma,
+    svgGithub: svgGithub,
+    svgGithub2: svgGithub2,
+    svgHtml: svgHtml,
+    svgIllustrator: svgIllustrator,
+    svgInstagram: svgInstagram,
+    svgJs: svgJs,
+    svgLinkedin: svgLinkedin,
+    svgLogo: svgLogo,
+    svgMail: svgMail,
+    svgNode: svgNode,
+    svgPhotoshop: svgPhotoshop,
+    svgReact: svgReact,
+    svgSai: svgSai,
+    svgSass: svgSass,
+    svgTiktok: svgTiktok,
+    svgTwiiter: svgTwiiter,
+    svgWhatsapp: svgWhatsapp
+};
+
+export default Images;
